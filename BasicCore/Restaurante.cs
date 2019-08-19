47ced5a220149821b74a2950ebfe4d68dae50eb2
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2019-08-19 07:15:57 -0300

    Criando uma entidade
    Foi criada a entidade restaurante.

---
+++ BasicCore/Restaurante.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicCore
+{
+    public class Restaurante
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public TipoCozinha Cozinha { get; set; }
+    }
+}
---
+++ BasicCore/TipoCozinha.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicCore
+{
+    public enum TipoCozinha
+    {
+        Nenhum,
+        Mexicana,
+        Italiana,
+        Brasileira
+    }
+}
