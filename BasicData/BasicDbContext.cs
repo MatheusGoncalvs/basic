32b710b5510259a9b4af261adb249fa871cf0189
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2019-09-03 07:20:54 -0300

    implementando um dbcontext

---
+++ BasicData/BasicDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BasicCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicData
+{
+    class BasicDbContext : DbContext
+    {
+        public DbSet<Restaurante> restaurantes { get; set; }
+    }
+}
