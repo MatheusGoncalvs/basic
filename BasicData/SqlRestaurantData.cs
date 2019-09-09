937ed527f8283559c036362f3f65fc15bfba4626
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2019-09-09 07:18:01 -0300

    Implementada a classe SqlRestaurantData
    +Através do configure services, o aspnet core vai se deparar com algum componente ou como uma página Razor que precise de algo que implemente IRestaurantData. O configure services passará o DbContext para a class SqlRestaurantData : IRestaurantData

---
+++ BasicData/SqlRestaurantData.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using BasicCore;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicData
+{
+    public class SqlRestaurantData : IRestauranteData
+    {
+        private readonly BasicDbContext db;
+
+        public SqlRestaurantData(BasicDbContext db)
+        {
+            this.db = db;
+        }
+
+        public Restaurante Add(Restaurante newRestaurant)
+        {
+            db.Add(newRestaurant);
+            return newRestaurant;
+        }
+
+        public int Commit()
+        {
+            return db.SaveChanges();
+        }
+
+        public Restaurante Delete(int id)
+        {
+            var restaurant = GetById(id);
+            if(restaurant != null)
+            {
+                db.restaurantes.Remove(restaurant);
+            }
+            return restaurant;
+        }
+
+        public Restaurante GetById(int id)
+        {
+            return db.restaurantes.Find(id);
+        }
+
+        public IEnumerable<Restaurante> GetRestaurantsByName(string name)
+        {
+            var query = from r in db.restaurantes
+                        where r.Name.StartsWith(name) || string.IsNullOrEmpty(name)
+                        orderby r.Name
+                        select r;
+            return query;
+        }
+
+        public Restaurante Update(Restaurante updatedRestaurant)
+        {
+            var entity = db.restaurantes.Attach(updatedRestaurant);
+            entity.State = EntityState.Modified;
+            return updatedRestaurant;
+        }
+    }
+}
